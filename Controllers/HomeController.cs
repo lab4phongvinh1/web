94b4996ea73ee58939f3b8f29d200350b26b198c
01232210125 <thanhphong9718@gmail.com>
2019-03-22 15:04:00 +0700

    sua

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace webshoppingonline.Controllers
             return View();
         }
 
-        public ActionResult LienHe()
+        public ActionResult lienhevoi()
         {
             ViewBag.Message = "Your contact page.";
 
